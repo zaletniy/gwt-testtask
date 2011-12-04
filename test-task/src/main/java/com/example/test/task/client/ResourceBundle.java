45a24eea5b7cd8521786cf7d11f2a4d69827aa3c
Ilya Sviridov <sviridov.ilya@gmail.com>
2011-12-04 21:06:27 +0200

    Tabs are added. But still styling needed

--- test-task/src/main/java/com/example/test/task/client/ResourceBundle.java
+++ test-task/src/main/java/com/example/test/task/client/ResourceBundle.java
@@ -2,21 +2,16 @@ package com.example.test.task.client;
 
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.resources.client.ClientBundle;
+import com.google.gwt.resources.client.CssResource;
 import com.google.gwt.resources.client.DataResource;
 import com.google.gwt.resources.client.ImageResource;
-import com.google.gwt.user.cellview.client.CellTable;
 
 public interface ResourceBundle extends ClientBundle {
     public static final ResourceBundle INSTANCE = GWT.create(ResourceBundle.class);
     
-    /*
+    
 	@Source("ApplicationStyle.css")
 	public Style css();
-	*/
-    
-    @Source({ CellTable.Style.DEFAULT_CSS,
-	"com/example/test/task/client/SubstitutionManagementTable.css" })
-    CellTable.Style cellTableStyle();
 	
 	@Source("create.gif")
 	public ImageResource createIcon();
@@ -25,12 +20,14 @@ public interface ResourceBundle extends ClientBundle {
 	public ImageResource substitutionWindowIcon();
 	
 	@Source("close_btn.gif")
-	public ImageResource closeButton();
+	public ImageResource closeButtonIcon();
 	
-	@Source("table_heading_bg.gif")
-	DataResource tableHeaderBackground();
+	@Source("close_btn.gif")
+	public DataResource closeButtonIconResource();
 	
-	@Source("cell_bg.gif")
-	DataResource firstColumnBackground();
-	
+	public interface Style extends CssResource {
+	    String closeButton();
+	    String tabStyle();
+    }
+
 }
